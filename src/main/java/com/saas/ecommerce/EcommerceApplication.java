91915b6bb35b093d322ed5618b3bfcb195969562
adarshmau <adarshmaurya13@gmail.com>
2026-04-30 13:27:39 +0530

    phase 1: project setup complete

---
+++ src/main/java/com/saas/ecommerce/EcommerceApplication.java
@@ -0,0 +1,18 @@
+package com.saas.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+
+        SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
